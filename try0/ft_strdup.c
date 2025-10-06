3b46f14c23bd281c0c0b4ac9e3641ba7c3ebfdf9
Raul <carlospicon80@gmail.com>
2025-10-06 18:32:47 +0200

    subida archivos

--- try0/ft_strdup.c
+++ try0/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: raulp <raulp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 14:58:24 by raulp             #+#    #+#             */
-/*   Updated: 2025/10/06 15:14:50 by raulp            ###   ########.fr       */
+/*   Updated: 2025/10/06 15:38:58 by raulp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,44 @@
 char *ft_strdup(const char *string)
 {
 	size_t i;
+	size_t len;
+	char *arr;
+	i = 0;
 
-	i=0;
-	while(string[i])
+	len = ft_strlen(string);
+
+	arr = malloc(len);
+
+	while (string[i])
 	{
-		
+		arr[i] = string[i];
+		i++;
 	}
+	arr[i] = '\0';
+	return arr;
+}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    const char *original = "Hola Mundo";
+    char *copia;
+
+    copia = ft_strdup(original); // duplicar cadena
+    if (copia == NULL) {
+        printf("Error al duplicar la cadena\n");
+        return 1;
+    }
+
+    printf("Original: %s\n", original);
+    printf("Copia: %s\n", copia);
+
+    // Modificar la copia para verificar que son independientes
+    copia[0] = 'h';
+    printf("Copia modificada: %s\n", copia);
+    printf("Original después de modificar la copia: %s\n", original);
+
+    free(copia); // liberar memoria
+    return 0;
 }
